2950440b51495d458dca4bccccba3cade1e57df3
innlouvate <l.j.franklin85@googlemail.com>
2016-03-16 10:23:14 +0000

    db survey relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316100737) do
+ActiveRecord::Schema.define(version: 20160316102115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,17 @@ ActiveRecord::Schema.define(version: 20160316100737) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "survey_id"
+  end
+
+  add_index "questions", ["survey_id"], name: "index_questions_on_survey_id", using: :btree
+
+  create_table "surveys", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "surveys"
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 describe Question, type: :model do
   it { is_expected.to have_many :answers }
   # it { is_expected.to have_many :users }
-  # it { is_expected.to belong_to :survey }
+  it { is_expected.to belong_to :survey }
 end
