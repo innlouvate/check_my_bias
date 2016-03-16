ea3cdfcc4818ac65eff9b80116dce72880612fbc
esbaddeley <esbaddeley@gmail.com>
2016-03-16 12:12:44 +0000

    Created a user model with devise and added name, age and gender columns Closes #11

---
+++ db/migrate/20160316120830_add_fileds_to_users.rb
@@ -0,0 +1,7 @@
+class AddFiledsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :age, :integer
+    add_column :users, :gender, :string
+  end
+end
--- spec/models/survey_spec.rb
+++
@@ -1,5 +0,0 @@
-# require 'rails_helper'
-#
-# describe Survey, type: :model do
-#   it { is_expected.to have_many :questions }
-# end
