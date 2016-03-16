51c55badf1a4ee935e4ff21cbc9d1c6f243ccece
innlouvate <l.j.franklin85@googlemail.com>
2016-03-16 19:21:48 +0000

    produces json with q&a - needs to be refactored into quiz class or similar

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,8 +3,12 @@ require 'json'
 class QuestionsController < ApplicationController
 
   def index
-    @quiz = Question.all
-    render :json => @quiz.as_json
+    @questions = Question.all
+    @quiz = {}
+    @questions.each do |q|
+      @quiz[q.content] = Answer.where(question_id: q).as_json
+    end
+    render :json => @quiz
   end
 
 
--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,9 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   has_many :responses
+
+  def as_json
+    super(:only => [:content])
+  end
+
 end
