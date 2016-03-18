ec5a9f45f0d800d1d48046f14b3bb234dab75281
esbaddeley <esbaddeley@gmail.com>
2016-03-18 12:52:51 +0000

    Seeded the database with the questions and answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  root to: 'application#angular'
 
   devise_for :users
 
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20160316150722) do
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
-    t.string   "content"
+    t.text     "content"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "question_id"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20160316150722) do
   add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
   create_table "questions", force: :cascade do |t|
-    t.string   "content"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
